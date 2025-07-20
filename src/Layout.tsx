c73be74fa82464f354f9367f729a7cbeda52aa07
Islam-Khairy <islamkhairy12@gmail.com>
2025-07-20 11:41:51 +0300

    solving pwa issue 3

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = React.memo(({ children }: any) => {
   return (
     <div className='flex h-screen'>
       <Sidebar />
-      <div className={`flex flex-col w-full h-full overflow-y-auto scroll-container`}>
+      <div className={`flex flex-col w-full h-full overflow-y-auto scroll-container mb-4 md:mb-0`}>
         <Header />
         <div className='md:p-4'>{children}</div>
       </div>
