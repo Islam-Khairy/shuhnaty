d775d29ee3b3c75e7ae61f65497c9f63b5693cc2
Islam-Khairy <islamkhairy12@gmail.com>
2025-09-26 09:53:42 +0300

    footer optimized styles || 4

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,31 +20,28 @@ const Login = () => {
   };
 
   return (
-    <div className='flex flex-col justify-between items-center w-screen h-screen max-h-screen overflow-y-hidden'>
+    <div className='flex justify-center items-center w-screen h-screen max-h-screen overflow-y-hidden'>
       {isLoading && (
         <div className={`fixed inset-0 flex justify-center items-center z-50 bg-opacity-15`}>
           <span className='loader'></span>
         </div>
       )}
-
-      <div className='flex-1'></div>
-
-      <div className='w-full max-w-lg shadow-2xl border bg-[#FCFCFC] mx-2 md:mx-0 px-8 py-10 rounded-xl'>
-        <div className='flex justify-center items-center mb-6'>
+      <div className='w-full max-w-lg shadow-2xl border bg-[#FCFCFC] mx-2 md:mx-0 px-8 py-6 rounded-xl -mt-12'>
+        <div className='flex justify-center items-center mb-4'>
           <img
             src={logo}
             alt='logo'
             className='w-24 h-24'
           />
         </div>
-        <h1 className='mb-8 text-center font-medium text-xl'>الدخول للحساب</h1>
+        <h1 className='my-6 text-center font-medium text-xl'>الدخول للحساب</h1>
         <form
           onSubmit={handleSubmit}
           className='flex flex-col items-start gap-1'
         >
           <label
             htmlFor='userName'
-            className='text-[#333333] text-sm mb-2'
+            className='text-[#333333] text-sm'
           >
             اسم المستخدم
           </label>
@@ -52,28 +49,27 @@ const Login = () => {
             <input
               id='userName'
               dir='rtl'
-              className='border border-gray-200 w-full rounded-md py-3 ps-10 focus:outline-none font-Rubik'
+              className='border border-gray-200 w-full rounded-md py-2 ps-10 focus:outline-none font-Rubik'
               placeholder='أدخل اسم المستخدم'
             />
             <CgProfile
               size={20}
               color='#666666'
-              className='absolute right-3 top-1/2 transform -translate-y-1/2'
+              className='absolute right-3 top-1/2 transform -translate-y-1/2 '
             />
           </div>
-
           <label
             htmlFor='password'
-            className='text-[#333333] text-sm mb-2'
+            className='text-[#333333] text-sm'
           >
             كلمة المرور
           </label>
-          <div className='relative w-full mb-6'>
+          <div className='relative w-full mb-4'>
             <input
               id='password'
               type={isPasswordVisible ? 'text' : 'password'}
               dir='rtl'
-              className='border border-gray-200 w-full rounded-md py-3 ps-10 focus:outline-none font-Rubik'
+              className='border border-gray-200 w-full rounded-md py-2 ps-10 focus:outline-none font-Rubik'
               placeholder='أدخل كلمة المرور'
             />
             <CiLock
@@ -100,18 +96,15 @@ const Login = () => {
               )}
             </button>
           </div>
-
-          <span className='font-Rubik text-[#333333] text-sm mb-6'>
+          <span className='font-Rubik text-[#333333] text-sm'>
             هذه نسخة تجريبية، فقط قم بالضغط على زر تسجيل الدخول
           </span>
-
-          <button className='w-full px-10 py-3 text-[#FCFCFC] font-thin bg-[#DD7E1F] rounded-lg'>
+          <button className='w-full px-10 py-2 text-[#FCFCFC] font-thin bg-[#DD7E1F] rounded-lg mt-4'>
             تسجيل الدخول
           </button>
         </form>
       </div>
-
-      <div className='flex-1 flex items-end pb-6'>
+      <div className='absolute bottom-6 w-full text-center'>
         <p className='text-gray-500 text-sm font-Rubik'>
           © {new Date().getFullYear()} Shuhnaty Dashboard Demo by{' '}
           <a
