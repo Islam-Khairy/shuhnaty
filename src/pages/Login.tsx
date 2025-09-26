d533441477323f7274d5838e02ae34823b7d3f3a
Islam-Khairy <islamkhairy12@gmail.com>
2025-09-26 09:36:35 +0300

    optimized styles || 2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,103 +20,107 @@ const Login = () => {
   };
 
   return (
-    <div className='flex justify-center items-center w-screen h-screen max-h-screen overflow-y-hidden'>
+    <>
       {isLoading && (
         <div className={`fixed inset-0 flex justify-center items-center z-50 bg-opacity-15`}>
           <span className='loader'></span>
         </div>
       )}
-      <div className='w-full max-w-lg shadow-2xl border bg-[#FCFCFC] mx-2 md:mx-0 px-8 py-6 rounded-xl -mt-6'>
-        <div className='flex justify-center items-center mb-4'>
-          <img
-            src={logo}
-            alt='logo'
-            className='w-24 h-24'
-          />
-        </div>
-        <h1 className='my-6 text-center font-medium text-xl'>الدخول للحساب</h1>
-        <form
-          onSubmit={handleSubmit}
-          className='flex flex-col items-start gap-1'
-        >
-          <label
-            htmlFor='userName'
-            className='text-[#333333] text-sm'
-          >
-            اسم المستخدم
-          </label>
-          <div className='relative w-full mb-6'>
-            <input
-              id='userName'
-              dir='rtl'
-              className='border border-gray-200 w-full rounded-md py-2 ps-10 focus:outline-none font-Rubik'
-              placeholder='أدخل اسم المستخدم'
-            />
-            <CgProfile
-              size={20}
-              color='#666666'
-              className='absolute right-3 top-1/2 transform -translate-y-1/2 '
-            />
-          </div>
-          <label
-            htmlFor='password'
-            className='text-[#333333] text-sm'
-          >
-            كلمة المرور
-          </label>
-          <div className='relative w-full mb-4'>
-            <input
-              id='password'
-              type={isPasswordVisible ? 'text' : 'password'}
-              dir='rtl'
-              className='border border-gray-200 w-full rounded-md py-2 ps-10 focus:outline-none font-Rubik'
-              placeholder='أدخل كلمة المرور'
-            />
-            <CiLock
-              size={20}
-              color='#666666'
-              className='absolute right-3 top-1/2 transform -translate-y-1/2'
-            />
-            <button
-              type='button'
-              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-              className='absolute left-4 top-1/2 transform -translate-y-1/2'
-              aria-label={isPasswordVisible ? 'Hide password' : 'Show password'}
+      <div className='grid grid-rows-[1fr_auto] w-screen h-screen'>
+        <div className='flex justify-center items-center'>
+          <div className='w-full max-w-lg shadow-2xl border bg-[#FCFCFC] mx-2 md:mx-0 px-8 py-6 rounded-xl'>
+            <div className='flex justify-center items-center mb-4'>
+              <img
+                src={logo}
+                alt='logo'
+                className='w-24 h-24'
+              />
+            </div>
+            <h1 className='my-6 text-center font-medium text-xl'>الدخول للحساب</h1>
+            <form
+              onSubmit={handleSubmit}
+              className='flex flex-col items-start gap-1'
             >
-              {isPasswordVisible ? (
-                <IoEyeOffOutline
+              <label
+                htmlFor='userName'
+                className='text-[#333333] text-sm'
+              >
+                اسم المستخدم
+              </label>
+              <div className='relative w-full mb-6'>
+                <input
+                  id='userName'
+                  dir='rtl'
+                  className='border border-gray-200 w-full rounded-md py-2 ps-10 focus:outline-none font-Rubik'
+                  placeholder='أدخل اسم المستخدم'
+                />
+                <CgProfile
                   size={20}
                   color='#666666'
+                  className='absolute right-3 top-1/2 transform -translate-y-1/2 '
                 />
-              ) : (
-                <IoEyeOutline
+              </div>
+              <label
+                htmlFor='password'
+                className='text-[#333333] text-sm'
+              >
+                كلمة المرور
+              </label>
+              <div className='relative w-full mb-4'>
+                <input
+                  id='password'
+                  type={isPasswordVisible ? 'text' : 'password'}
+                  dir='rtl'
+                  className='border border-gray-200 w-full rounded-md py-2 ps-10 focus:outline-none font-Rubik'
+                  placeholder='أدخل كلمة المرور'
+                />
+                <CiLock
                   size={20}
                   color='#666666'
+                  className='absolute right-3 top-1/2 transform -translate-y-1/2'
                 />
-              )}
-            </button>
+                <button
+                  type='button'
+                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                  className='absolute left-4 top-1/2 transform -translate-y-1/2'
+                  aria-label={isPasswordVisible ? 'Hide password' : 'Show password'}
+                >
+                  {isPasswordVisible ? (
+                    <IoEyeOffOutline
+                      size={20}
+                      color='#666666'
+                    />
+                  ) : (
+                    <IoEyeOutline
+                      size={20}
+                      color='#666666'
+                    />
+                  )}
+                </button>
+              </div>
+              <span className='font-Rubik text-[#333333] text-sm'>
+                هذه نسخة تجريبية، فقط قم بالضغط على زر تسجيل الدخول
+              </span>
+              <button className='w-full px-10 py-2 text-[#FCFCFC] font-thin bg-[#DD7E1F] rounded-lg mt-4'>
+                تسجيل الدخول
+              </button>
+            </form>
           </div>
-          <span className='font-Rubik text-[#333333] text-sm'>
-            هذه نسخة تجريبية، فقط قم بالضغط على زر تسجيل الدخول
-          </span>
-          <button className='w-full px-10 py-2 text-[#FCFCFC] font-thin bg-[#DD7E1F] rounded-lg mt-4'>
-            تسجيل الدخول
-          </button>
-        </form>
+        </div>
+        <div className='py-6 w-full text-center'>
+          <p className='text-gray-500 text-sm font-Rubik'>
+            © {new Date().getFullYear()} Shuhnaty Dashboard Demo by{' '}
+            <a
+              target='_blank'
+              href='https://www.linkedin.com/in/islam-khairy-364793307/'
+              className='text-[#DD7E1F] hover:underline'
+            >
+              Islam Khairy
+            </a>
+          </p>
+        </div>
       </div>
-      <div className='absolute bottom-8 w-full text-center'>
-        <p className='text-gray-500 text-sm font-Rubik'>
-          © {new Date().getFullYear()} Shuhnaty Dashboard Demo by{' '}
-          <a
-            target='_blank'
-            href='https://www.linkedin.com/in/islam-khairy-364793307/'
-            className='text-[#DD7E1F] hover:underline'
-          >
-            Islam Khairy
-          </a>
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
 
